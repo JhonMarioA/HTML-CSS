8a6f97bbe6696fa008252091fe9979fff957ac7b
Jhon Mario <jhon.aguirre1@utp.edu.co>
2025-06-16 15:56:21 -0500

    Add JavaScript file for basic calculations

---
+++ JS/PR1.js
@@ -0,0 +1,54 @@
+console.log("=================================")
+console.log("        ICE CREAM PARLOR         ")
+console.log("---------------------------------")
+console.log("          Order Details          ")
+console.log("---------------------------------")
+console.log("Item          Quantity     Price")
+console.log("---------------------------------")
+console.log("Ice cream gd       2        6.000")
+console.log("Ice cream po       1        4.000")
+console.log("---------------------------------")
+console.log("Total:                     16.000")
+console.log("=================================")
+
+
+
+// --------------------------------------------------------------------------------
+let mass = 96
+let height = 1.85
+bmi = mass/height ** 2
+
+console.log(bmi)
+
+
+
+// --------------------------------------------------------------------------------
+const Fahrenheit = 69
+celsius = (Fahrenheit - 32)/1.8
+
+console.log(celsius)
+
+
+
+// --------------------------------------------------------------------------------
+let earthWeight = 210 // weight on pounds
+
+let marsWeight = earthWeight * 0.38
+
+console.log("Your weight on Earth is ", earthWeight, " pounds.")
+console.log("Your weight on Mars is ", marsWeight, " pounds.")
+
+
+
+// --------------------------------------------------------------------------------
+let num = Math.random();
+
+if (num > 0.5) {
+  console.log("Heads");
+} else {
+  console.log("Tails");
+}
+
+
+// --------------------------------------------------------------------------------
+
