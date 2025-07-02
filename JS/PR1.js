979558315cf1e5ad19e77b4c125438d234023f2d
Jhon Mario <jhon.aguirre1@utp.edu.co>
2025-07-02 15:58:14 -0500

    funct

--- JS/PR1.js
+++ JS/PR1.js
@@ -259,8 +259,114 @@ const musicPlaylist = [
 musicPlaylist.shift()
 musicPlaylist.pop()
 
-musicPlaylist.push("La pela con el diablo")
+musicPlaylist.push("La pelea con el diablo")
 musicPlaylist.unshift("Hola beba remix")
 musicPlaylist.unshift("Osito gominola")
 
 console.log(musicPlaylist)
+
+// -----------------------------------------------------
+
+const characters = [
+  "The Wally Watchers",
+  "Wilma",
+  "Fritz",
+  "Wizard Whitebeard",
+  "Odlaw",
+  "Waldo",
+  "Woof"
+];
+
+if(characters.includes("Waldo")){
+
+  let waldoIndex = characters.indexOf("Waldo")
+  console.log("Found Waldo at index " + waldoIndex)
+
+} else{
+  console.log("Not found")
+}
+
+// -----------------------------------------------------
+
+const dnaPieces = ["A", "C", "G", "T"]
+let myDNA = []
+let cont = 1
+
+
+while(cont <= 24){
+
+  let word = ""
+
+  for(let i=1; i<=3; i++){
+   
+      let index = Math.floor(Math.random() * 4)
+      let indexL = dnaPieces[index]
+      word += indexL
+
+  }
+
+  myDNA.push(word)
+  cont++
+}
+
+console.log(myDNA)
+
+// -----------------------------------------------------
+
+const recentTikTokViews = [1932, 2300, 453, 5222, 6733, 7402, 8334];
+const recentInstagramViews = [936, 2576, 453, 7013, 5489, 7402, 3921];
+const recentYouTubeViews = [2300, 453, 5222, 989, 6733, 7402, 2789];
+
+function mean(viewsArray) {
+  let totalViews = 0;
+
+  for(let i = 0; i < viewsArray.length; i++) {
+    totalViews += viewsArray[i];
+  }
+
+  return totalViews / viewsArray.length;
+}
+
+function median(viewsArray) {
+  const sortedStats = viewsArray.sort(function(a, b) { return a - b; });
+  const middleIndex = Math.floor(viewsArray.length / 2);
+  
+  return sortedStats[middleIndex];
+}
+
+// TikTok
+console.log("TikTok");
+console.log("Mean:", mean(recentTikTokViews));
+console.log("Median:", median(recentTikTokViews));
+console.log();
+
+// Instagram
+console.log("Instagram");
+console.log("Mean:", mean(recentInstagramViews));
+console.log("Median:", median(recentInstagramViews));
+console.log();
+
+// YouTube
+console.log("YouTube");
+console.log("Mean:", mean(recentYouTubeViews));
+console.log("Median:", median(recentYouTubeViews));
+console.log();
+// ----------------------------------------------------- functions practice
+
+
+isPalindrome("racecar")
+
+function isPalindrome(word) {
+  let wordB = word
+  let wordM = ""
+
+  for (let i = 0; i < word.length; i++) {
+    wordM = wordB[i] + wordM 
+  }
+
+  if (word === wordM) {
+    console.log("Is a palindrome")
+  } else {
+    console.log("Is not a palindrome")
+  }
+}
